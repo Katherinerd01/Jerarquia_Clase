b4d5146a0fbd6e1889a5aa4c781f6b139c25b9cf
Katherinerd01 <katherinerdd01@gmail.com>
2024-06-05 20:38:32 -0400

    Modulo 5

---
+++ Jerarquia_de_Clase/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+        public class MiembroDeLaComunidad
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+
+            public virtual void MostrarInformacion()
+            {
+                Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+            }
+        }
+
+        public class Empleado : MiembroDeLaComunidad
+        {
+            public string Puesto { get; set; }
+
+            public override void MostrarInformacion()
+            {
+                base.MostrarInformacion();
+                Console.WriteLine($"Puesto: {Puesto}");
+            }
+        }
+
+        public class Estudiante : MiembroDeLaComunidad
+        {
+            public string Carrera { get; set; }
+
+            public override void MostrarInformacion()
+            {
+                base.MostrarInformacion();
+                Console.WriteLine($"Carrera: {Carrera}");
+            }
+        }
+
+        public class ExAlumno : MiembroDeLaComunidad
+        {
+            public int AñoGraduacion { get; set; }
+
+            public override void MostrarInformacion()
+            {
+                base.MostrarInformacion();
+                Console.WriteLine($"Año de Graduación: {AñoGraduacion}");
+            }
+        }
+
+        public class Docente : Empleado
+        {
+        }
+
+        public class Administrativo : Empleado
+        {
+        }
+
+        public class Administrador : Docente
+        {
+        }
+
+        public class Maestro : Docente
+        {
+        }
+   
+
+
+
